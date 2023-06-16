8d429f09e82afcd3ac4f8cb2f3b5147d41be60ea
Ann <ann.mitrakhovich@gmail.com>
2023-06-16 10:15:17 +0300

    Used for to print counts

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int[] InputSomeNums()
+{
+    Console.Write("Введите количество чисел: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+        else
+            Console.Write(".");
+    }
+}
+
+int[] myArray = InputSomeNums();
+PrintArray(myArray);
